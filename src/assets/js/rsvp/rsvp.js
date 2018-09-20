a65497d55399ed34bb682511f8912c27541e4645
John Ferguson <snobord4life@gmail.com>
2018-09-20 10:31:53 -0600

    edit to rsvp

--- src/assets/js/rsvp/rsvp.js
+++ src/assets/js/rsvp/rsvp.js
@@ -2,13 +2,14 @@ var $form = $('form#rsvpform'),
     url = 'https://script.google.com/macros/s/AKfycbzoPQoLokSVqlN1tQ4k5o5jN3GdY0VvOW2rv5g2Grm1okgZ52k/exec'
 
 $('#submitButton').on('click', function(e) {
-  e.preventDefault();
+  console.log("you clicked submit!")
+  /*e.preventDefault();
   var jqxhr = $.ajax({
     url: url,
     method: "GET",
     dataType: "json",
-    data: $form.serializeObject()
+    data: $form.serializeObject()*/
   }).success(
-    // do something
+
   );
 })
